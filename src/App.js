453ecc81c06725bcded4a47007f5e0011a193e24
delayedaa <delayedaa@gmail.com>
2020-12-09 08:50:08 -0600

    [App.js] Refactor App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import './App.css';
 import Movie from './Movie';
 
 function App() {
+  const movieData = [
+    {
+      title: 'The Fellowship of the Ring',
+      hours: '2',
+      minutes: '58'
+    },
+    {
+      title: 'The Two Towers',
+      hours: '2',
+      minutes: '59'
+    },
+    {
+      title: 'The Return of the King',
+      hours: '3',
+      minutes: '21'
+    }
+  ];
+
+  const movieComponents = movieData.map((movie, index) => {
+    // return <Movie title={movie.title} hours={movie.hours} minutes={movie.minutes} key={'movie-' + index} />
+    return <Movie {...movie} key={'movie-' + index} />;
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,21 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Movie
-          title="The Fellowship of the Ring"
-          hours="2"
-          minutes="58"
-        />
-        <Movie
-          title="The Two Towers"
-          hours="2"
-          minutes="59"
-        />
-        <Movie
-          title="The Return of the King"
-          hours="3"
-          minutes="21"
-        />
+        {movieComponents}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,9 @@ function Movie(props) {
   return (
     <div>
       <h3>The Lord of the Rings: {props.title}</h3>
-      <p>{props.hours}h {props.minutes}min</p>
+      <p>
+        {props.hours}h {props.minutes}min
+      </p>
     </div>
   );
 }
